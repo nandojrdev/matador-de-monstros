eeb8431aaadf99cda209538a041344928cd83127
Fernando Junior <fernandojuniordev@gmail.com>
2020-08-14 14:20:34 -0300

    Feature Iniciar Jogo e Desistir Implementadas

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 new Vue({
   el: '#app',
   data: {
-
+    running: false,
+    playerLife: 0,
+    monsterLife: 20,
   },
   computed: {
-
+    hasResult() {
+      return this.playerLife === 0 || this.monsterLife === 0
+    }
   },
   methods: {
-
+    startGame() {
+      this.running = true
+      this.playerLife = 100
+      this.monsterLife = 100
+    }
   },
   watch: {
-    
+
   }
 })
